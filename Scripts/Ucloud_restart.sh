9bb4a93beb7bb7a9caf3fc42d964140452c28765
joseale2310 <joseale2310@hotmail.es>
2022-05-21 11:24:30 +0200

    updated ucloud setup

--- Scripts/Ucloud_restart.sh
+++ Scripts/Ucloud_restart.sh
@@ -2,5 +2,6 @@
 
 echo "Sys.setenv(RENV_PATHS_CACHE = 'work/sandbox_bulkRNAseq_testAndFeedback/envs/cache')" > ~/.Rprofile
 echo "renv::load(project = '/work/sandbox_bulkRNAseq_testAndFeedback/envs')" >> ~/.Rprofile
+echo "renv::repair()" >> ~/.Rprofile
 
 printf "Done.\n"
--- Scripts/Ucloud_setup.sh
+++ Scripts/Ucloud_setup.sh
@@ -2,16 +2,20 @@
 
 echo "Sys.setenv(RENV_PATHS_CACHE = 'work/sandbox_bulkRNAseq_testAndFeedback/envs/cache')" > ~/.Rprofile
 echo "renv::load(project = '/work/sandbox_bulkRNAseq_testAndFeedback/envs')" >> ~/.Rprofile
+echo "renv::repair()" >> ~/.Rprofile
 
 mkdir introduction_bulkRNAseq_analysis
 cd introduction_bulkRNAseq_analysis
+mkdir "Results"
+
 git init
 git remote add origin https://github.com/hds-sandbox/bulk_RNAseq_course.git
 git config core.sparseCheckout true
 echo "Assignments/" >> .git/info/sparse-checkout
 echo "Environments/" >> .git/info/sparse-checkout
 echo "Notebooks/" >> .git/info/sparse-checkout
-echo "Scripts/" >> .git/info/sparse-checkout
 git pull --depth=1 origin spring_2022
+
+rm .gitignore
 
 printf "Done.\n"
