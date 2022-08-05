ecc6d914d3d540767e38f347a1997cf4a4057fb2
joseale2310 <joseale2310@hotmail.es>
2022-08-05 21:39:06 +0200

    Ucloud setup fix

--- Scripts/Ucloud_setup.sh
+++ Scripts/Ucloud_setup.sh
@@ -1,34 +1,22 @@
 #!/usr/bin
 
-echo "Sys.setenv(RENV_PATHS_CACHE = 'work/sandbox_bulkRNAseq_testAndFeedback/envs_4.2/cache')" > ~/.Rprofile
+echo "Sys.setenv(RENV_PATHS_CACHE = '/work/sandbox_bulkRNAseq_testAndFeedback/envs_4.2/cache')" > ~/.Rprofile
 echo "renv::load(project = '/work/sandbox_bulkRNAseq_testAndFeedback/envs_4.2')" >> ~/.Rprofile
 echo "renv::repair()" >> ~/.Rprofile
 
 
 FILE="introduction_bulkRNAseq_analysis"
-if [[ -f "$FILE" ]]; then
-    echo "$FILE exists. No overwriting."
+if [[ -d "$FILE" ]]; then
+	cd $FILE
+	ln -s ../sandbox_bulkRNAseq_testAndFeedback/bulk_RNAseq_course/Data/ ./Data
+	echo "$FILE exists. No overwriting."
 else
 	echo "No $FILE found. Creating new directory"
-	mkdir introduction_bulkRNAseq_analysis
-	cd introduction_bulkRNAseq_analysis
+	mkdir $FILE
+	cd $FILE
 	mkdir Results
 	cp -r /work/sandbox_bulkRNAseq_testAndFeedback/bulk_RNAseq_course/Notebooks ./
 	ln -s ../sandbox_bulkRNAseq_testAndFeedback/bulk_RNAseq_course/Data/ ./Data
 fi
-
-# mkdir introduction_bulkRNAseq_analysis
-# cd introduction_bulkRNAseq_analysis
-# mkdir "Results"
-
-# git init
-# git remote add origin https://github.com/hds-sandbox/bulk_RNAseq_course.git
-# git config core.sparseCheckout true
-# echo "Assignments/" >> .git/info/sparse-checkout
-# echo "Environments/" >> .git/info/sparse-checkout
-# echo "Notebooks/" >> .git/info/sparse-checkout
-# git pull --depth=1 origin spring_2022
-
-# rm .gitignore
 
 echo "Done.\n"
