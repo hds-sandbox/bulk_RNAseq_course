53f8157238bc02324b7fd28a02cd29045b7f40c4
joseale2310 <joseale2310@hotmail.es>
2022-08-08 11:01:24 +0200

    ucloud new project

--- Scripts/Ucloud_setup.sh
+++ Scripts/Ucloud_setup.sh
@@ -1,22 +1,22 @@
 #!/usr/bin
 
-echo "Sys.setenv(RENV_PATHS_CACHE = '/work/sandbox_bulkRNAseq_testAndFeedback/envs_4.2/cache')" > ~/.Rprofile
-echo "renv::load(project = '/work/sandbox_bulkRNAseq_testAndFeedback/envs_4.2')" >> ~/.Rprofile
+echo "Sys.setenv(RENV_PATHS_CACHE = '/work/82207/envs_4.2/cache')" > ~/.Rprofile
+echo "renv::load(project = '/work/82207/envs_4.2')" >> ~/.Rprofile
 echo "renv::repair()" >> ~/.Rprofile
 
 
 FILE="introduction_bulkRNAseq_analysis"
 if [[ -d "$FILE" ]]; then
 	cd $FILE
-	ln -s ../sandbox_bulkRNAseq_testAndFeedback/bulk_RNAseq_course/Data/ ./Data
+	ln -s ../82207/bulk_RNAseq_course/Data/ ./Data
 	echo "$FILE exists. No overwriting."
 else
 	echo "No $FILE found. Creating new directory"
 	mkdir $FILE
 	cd $FILE
 	mkdir Results
-	cp -r /work/sandbox_bulkRNAseq_testAndFeedback/bulk_RNAseq_course/Notebooks ./
-	ln -s ../sandbox_bulkRNAseq_testAndFeedback/bulk_RNAseq_course/Data/ ./Data
+	cp -r /work/82207/bulk_RNAseq_course/Notebooks ./
+	ln -s ../82207/bulk_RNAseq_course/Data/ ./Data
 fi
 
 echo "Done.\n"
