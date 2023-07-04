e6fe82f1806f4e1e643ada82f71c170ed1d2492c
joseale2310 <joseale2310@hotmail.es>
2023-07-04 09:22:32 +0200

    fix: bulk RNAseq workshop June 2023 hotfixes

--- Scripts/ucloud_preprocessing_setup/preprocessing.sh
+++ Scripts/ucloud_preprocessing_setup/preprocessing.sh
@@ -1,6 +1,3 @@
-mkdir preprocessing_results
-cp 778339/samplesheet.csv samplesheet.csv
-
+cp /work/778339/samplesheet.csv /work/samplesheet.csv
 cd /work
-
-nextflow run nf-core/rnaseq -r 3.11.2 -params-file /work/778339/Scripts/nf-params_salmon.json -profile conda --max_cpus 8 --max_memory 40GB
+nextflow run nf-core/rnaseq -r 3.11.2 -params-file /work/778339/Scripts/nf-params.json -profile conda --max_cpus 8 --max_memory 40GB
---
+++ Scripts/ucloud_preprocessing_setup/preprocessing_traditional.sh
@@ -0,0 +1,3 @@
+cp /work/778339/samplesheet.csv /work/samplesheet.csv
+cd /work
+nextflow run nf-core/rnaseq -r 3.11.2 -params-file /work/778339/Scripts/nf-params_traditional.json -profile conda --max_cpus 8 --max_memory 40GB
