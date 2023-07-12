a01147233cfeb9a14f8eef0e7163f54e5d27a81e
joseale2310 <joseale2310@hotmail.es>
2023-07-12 21:39:18 +0200

    feat: added star and salmon indexes to preprocessing pipelines

--- Scripts/ucloud_preprocessing_setup/preprocessing.sh
+++ Scripts/ucloud_preprocessing_setup/preprocessing.sh
@@ -5,3 +5,14 @@ cp /work/778339/Scripts/nf-params.json /work/nf-params.json
 cd /work
 
 nextflow run nf-core/rnaseq -r 3.11.2 -params-file /work/nf-params.json -profile conda --max_cpus 8 --max_memory 40GB
+
+# Search for the file recursively
+file_path=$(find / -name "multiqc_report.html" 2>/dev/null)
+
+# Check if the file exists
+if [[ -n "$file_path" ]]; then
+    # Clean run
+    nextflow clean
+    mv /work/nf-params.json /work/preprocessing_star_salmon/nf-params.json
+
+fi
--- Scripts/ucloud_preprocessing_setup/preprocessing_traditional.sh
+++ Scripts/ucloud_preprocessing_setup/preprocessing_traditional.sh
@@ -5,3 +5,14 @@ cp /work/778339/Scripts/nf-params_traditional.json /work/nf-params_traditional.j
 cd /work
 
 nextflow run nf-core/rnaseq -r 3.11.2 -params-file /work/nf-params_traditional.json -profile conda --max_cpus 8 --max_memory 40GB
+
+# Search for the file recursively
+file_path=$(find / -name "multiqc_report.html" 2>/dev/null)
+
+# Check if the file exists
+if [[ -n "$file_path" ]]; then
+    # Clean run
+    nextflow clean
+    mv /work/nf-params_traditional.json /work/preprocessing_traditional/nf-params_traditional.json
+
+fi
