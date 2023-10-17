77cd0d51af2fea58e49da0690b4fef8473a4d4ba
joseale2310 <joseale2310@hotmail.es>
2023-10-17 13:21:33 +0200

    post-debacle fixes

--- Scripts/ucloud_preprocessing_setup/preprocessing_salmon.sh
+++ Scripts/ucloud_preprocessing_setup/preprocessing_salmon.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cp /work/778339/samplesheet.csv /work/samplesheet.csv
+cp /work/778339/merge/samplesheet.csv /work/samplesheet.csv
 cp /work/778339/Scripts/nf-params_salmon.json /work/nf-params_salmon.json
 cd /work
 
 nextflow run nf-core/rnaseq -r 3.11.2 -params-file /work/nf-params_salmon.json -profile conda --max_cpus 8 --max_memory 40GB
 
 # Search for the file recursively
-file_path=$(find / -name "multiqc_report.html" 2>/dev/null)
+file_path=$(find results_salmon -name "multiqc_report.html" 2>/dev/null)
 
 # Check if the file exists
 if [[ -n "$file_path" ]]; then
--- Scripts/ucloud_preprocessing_setup/ref_genomes.sh
+++ Scripts/ucloud_preprocessing_setup/ref_genomes.sh
@@ -5,14 +5,14 @@
 # 2023-07-10
 
 cd /work/778339
-mkdir -p ref_genomes/human/GRCh38
-cd  /work/778339/ref_genomes/human/GRCh38
+mkdir -p genomic_resources/homo_sapiens/GRCh38
+cd  /work/778339/genomic_resources/homo_sapiens/GRCh38
 
 wget -L ftp://ftp.ensembl.org/pub/release-109/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa.gz
 wget -L ftp://ftp.ensembl.org/pub/release-109/gtf/homo_sapiens/Homo_sapiens.GRCh38.109.gtf.gz
 
-mkdir -p ref_genomes/mouse/GRCm39
-cd  /work/778339/ref_genomes/mouse/GRCm39
+mkdir -p genomic_resources/mus_musculus/GRCm39
+cd  /work/778339/genomic_resources/mus_musculus/GRCm39
 
 wget -L ftp://ftp.ensembl.org/pub/release-109/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna_sm.primary_assembly.fa.gz
 wget -L https://ftp.ensembl.org/pub/release-109/gtf/mus_musculus/Mus_musculus.GRCm39.109.gtf.gz
