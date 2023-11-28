49467863d11690551e8b65028e25d9f274305ec8
joseale2310 <joseale2310@hotmail.es>
2023-11-28 21:12:14 +0100

    feat: updated scripts to vampirium

--- Scripts/ucloud_preprocessing_setup/preprocessing.sh
+++ Scripts/ucloud_preprocessing_setup/preprocessing.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cp /work/778339/merge/samplesheet.csv /work/samplesheet.csv
-cp /work/778339/Scripts/nf-params.json /work/nf-params.json
+cp /work/778339/raw_reads/samplesheet.csv /work/samplesheet.csv
+cp /work/778339/Scripts/ucloud_preprocessing_setup/nf-params.json /work/nf-params.json
 cd /work
 
 nextflow run nf-core/rnaseq -r 3.11.2 -params-file /work/nf-params.json -profile conda --max_cpus 8 --max_memory 40GB
 
 # Search for the file recursively
-file_path=$(find results_star_salmon -name "multiqc_report.html" 2>/dev/null)
+file_path=$(find preprocessing_results_star_salmon -name "multiqc_report.html" 2>/dev/null)
 
 # Check if the file exists
 if [[ -n "$file_path" ]]; then
     # Clean run
     rm -r /work/work
-    mv /work/nf-params.json /work/preprocessing_star_salmon/nf-params.json
+    mv /work/nf-params.json /work/preprocessing_results_star_salmon/nf-params.json
 
 fi
--- Scripts/ucloud_preprocessing_setup/preprocessing_traditional.sh
+++ Scripts/ucloud_preprocessing_setup/preprocessing_traditional.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cp /work/778339/merge/samplesheet.csv /work/samplesheet.csv
-cp /work/778339/Scripts/nf-params_traditional.json /work/nf-params_traditional.json
+cp /work/778339/raw_reads/samplesheet.csv /work/samplesheet.csv
+cp /work/778339/Scripts/ucloud_preprocessing_setup/nf-params_traditional.json /work/nf-params_traditional.json
 cd /work
 
 nextflow run nf-core/rnaseq -r 3.11.2 -params-file /work/nf-params_traditional.json -profile conda --max_cpus 8 --max_memory 40GB
 
-# Search for the file recursively
-file_path=$(find results_traditional -name "multiqc_report.html" 2>/dev/null)
+# Search for the last file created by the pipeline, the multiqc_report, recursively
+file_path=$(find /work/preprocessing_results_traditional -name "multiqc_report.html" 2>/dev/null)
 
 # Check if the file exists
 if [[ -n "$file_path" ]]; then
-    # Clean run
+    # Clean run if the pipeline is completed
     rm -r /work/work
-    mv /work/nf-params_traditional.json /work/preprocessing_traditional/nf-params_traditional.json
+    mv /work/nf-params_traditional.json /work/preprocessing_results_traditional/nf-params_traditional.json
 
 fi
